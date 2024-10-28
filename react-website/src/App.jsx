6b20eb35fdd622149dc6058cdbf59c6bbc2a33e3
kinga.viola <b.kingaviola@gmail.com>
2024-10-28 18:06:15 +0100

    Create layout

--- react-website/src/App.jsx
+++ react-website/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from '../public/assets/logo.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='homepage-container'>
+        <header>
+          <img src={logo} alt='Logo'/>
+          <label>COMNICA</label>
+        </header>
+        <main>
+          <section className='title-section'>
+            <h1 className='title'>Comnica Signature</h1>
+            <p>Digitális aláíró szolgáltatás a Comnicától</p>
+          </section>
+          <section className='description-section'>
+            <h2 className='sub-title'>Üdvözlöm a<br />Comnica Signature-ben!</h2>
+            <p>A következőkben végigvezetjük dokumentumai elfogadásán és aláírásán. A folyamat több percet is igénybe vehet, ezért kérjük, csak akkor kezdje el, ha készen áll rá.</p>
+          </section>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
